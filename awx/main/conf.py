6d9b2d9f41d3741f21cc9c9838608237e7cd2dd3
Seth Foster <fosterseth@users.noreply.github.com>
2025-10-10 15:44:37 -0400

    Make subscriptions credentials mutually exclusive (#7135)
    settings.SUBSCRIPTIONS_USERNAME and
    settings.SUBSCRIPTIONS_CLIENT_ID
    
    should be mutually exclusive. This is because
    the POST to api/v2/config/attach/ accepts only
    a subscription_id, and infers which credentials to
    use based on settings. If both are set, it is ambiguous
    and can lead to unexpected 400s when attempting
    to attach a license.
    
    Signed-off-by: Seth Foster <fosterbseth@gmail.com>

--- awx/main/conf.py
+++ awx/main/conf.py
@@ -158,6 +158,7 @@ register(
     help_text=_('Username used to retrieve subscription and content information'),  # noqa
     category=_('System'),
     category_slug='system',
+    hidden=True,
 )
 
 register(
@@ -171,6 +172,7 @@ register(
     help_text=_('Password used to retrieve subscription and content information'),  # noqa
     category=_('System'),
     category_slug='system',
+    hidden=True,
 )
 
 
@@ -185,6 +187,7 @@ register(
     help_text=_('Client ID used to retrieve subscription and content information'),  # noqa
     category=_('System'),
     category_slug='system',
+    hidden=True,
 )
 
 register(
@@ -198,6 +201,7 @@ register(
     help_text=_('Client secret used to retrieve subscription and content information'),  # noqa
     category=_('System'),
     category_slug='system',
+    hidden=True,
 )
 
 register(
